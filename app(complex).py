cd8ba36e4e6b09898c075bc2fc0135df12820457
zding5 <dzq@dzqs.local>
2015-03-05 01:12:33 -0600

    A

--- app(complex).py
+++ app(complex).py
@@ -3,7 +3,7 @@ from flask import Flask, request, redirect, url_for,render_template
 from werkzeug import secure_filename
 
 UPLOAD_FOLDER = './uploads'
-ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'doc', 'docx','png', 'jpg', 'jpeg', 'gif'])
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'doc', 'docx','png', 'jpg', 'jpeg', 'gif', 'ppt'])
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
@@ -19,11 +19,11 @@ def allowed_file(filename):
 def upload_file():
     if request.method == 'POST':
         file = request.files['file']
-        print request.form["committee"], request.form["title"]
+        print request.form["committee"], request.form["title"], request.form["number"]
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             # print os.path.join(app.config['UPLOAD_FOLDER'], filename)
-            newfilename = "%s-%s-%s"%(request.form["committee"],request.form["title"],filename)
+            newfilename = "%s--%s--%s"%(request.form["committee"],request.form["title"],request.form["number"]+" copies")
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], newfilename))
             return redirect('/')
     path = os.path.join(app.config['UPLOAD_FOLDER'])
